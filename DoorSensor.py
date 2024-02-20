981639238140dab6b1aa3b36995f423315fa28ec
Joseph Huntley <Joseph.Huntley@outlook.com>
2024-02-20 11:12:42 -0500

    Separated door sensor functions in unique file

---
+++ DoorSensor.py
@@ -0,0 +1,28 @@
+import json
+
+# Door sensor entity ID
+DOOR_SENSOR = "binary_sensor.front_door_sensor_opening"
+
+async def subscribe_to_door_sensor(ws):
+    subscribe_command = {
+        "id": 1,
+        "type": "subscribe_trigger",
+        "trigger": {
+            "platform": "state",
+            "entity_id": DOOR_SENSOR,
+            "from": "off",
+            "to": "on"
+        }
+    }
+    await ws.send(json.dumps(subscribe_command))
+    response = await ws.recv()
+    print(f"Subscription response: {response}")
+
+async def handle_door_sensor_events(ws, config_data):
+    # Implement logic here
+    while True:
+        res = await ws.recv()
+        # print(res)
+        if json.loads(res)["event"]["variables"]["trigger"]["entity_id"] == DOOR_SENSOR and armed_mode == "armed":
+            sensor = json.loads(res)["event"]["variables"]["trigger"]["to_state"]["attributes"]["friendly_name"]
+            send_notification(config_data, sensor + " has been opened")
--- main.py
+++ main.py
@@ -5,10 +5,8 @@ import requests
 import os
 from configparser import ConfigParser
 from Notifications import send_notification
+from DoorSensor import subscribe_to_door_sensor, handle_door_sensor_events
 from time import sleep
-
-# Door sensor entity ID
-DOOR_SENSOR = "binary_sensor.front_door_sensor_opening"
 
 # Set initial state
 armed_mode = "armed"
@@ -32,30 +30,6 @@ async def authenticate(ws, config_data):
         return False
     print("Authentication successful.")
     return True
-
-async def subscribe_to_door_sensor(ws):
-    subscribe_command = {
-        "id": 1,
-        "type": "subscribe_trigger",
-        "trigger": {
-            "platform": "state",
-            "entity_id": DOOR_SENSOR,
-            "from": "off",
-            "to": "on"
-        }
-    }
-    await ws.send(json.dumps(subscribe_command))
-    response = await ws.recv()
-    print(f"Subscription response: {response}")
-
-async def handle_door_sensor_events(ws, config_data):
-    # Implement logic here
-    while True:
-        res = await ws.recv()
-        # print(res)
-        if json.loads(res)["event"]["variables"]["trigger"]["entity_id"] == DOOR_SENSOR and armed_mode == "armed":
-            sensor = json.loads(res)["event"]["variables"]["trigger"]["to_state"]["attributes"]["friendly_name"]
-            send_notification(config_data, sensor + " has been opened")
 
 async def connect_to_ha_server(config_data):
     async with websockets.connect(config_data['HA_WS_URL']) as ws:
