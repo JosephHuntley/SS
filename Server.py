d26e8c239b5fa0d6dd066aff701d44d6d260a84e
Joseph Huntley <Joseph.Huntley@outlook.com>
2024-02-22 10:14:38 -0500

    Imported sleep to fix bug

--- Server.py
+++ Server.py
@@ -5,6 +5,7 @@ import websockets
 from door_sensor import subscribe_to_door_sensor, handle_door_sensor_events
 from logging_config import configure_logging, load_config
 from alarm_control import subscribe_to_armed_mode, handle_armed_mode_change, get_armed_mode
+import sleep
 
 # Load configuration
 config_data = load_config()
