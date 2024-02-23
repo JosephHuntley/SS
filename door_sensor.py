246c93dc28ac1622fa670dcf1bb01ff2fcb739f6
Joseph Huntley <Joseph.Huntley@outlook.com>
2024-02-22 21:03:24 -0500

    Added multiple sensors

--- door_sensor.py
+++ door_sensor.py
@@ -7,7 +7,7 @@ from alarm_control import ArmedMode
 configure_logging()
 
 # Door sensor entity ID
-DOOR_SENSOR = "binary_sensor.front_door_sensor_opening"
+SENSORS = ["binary_sensor.window_opening", "binary_sensor.front_door_sensor_opening",  "binary_sensor.sliding_door_opening"]
 
 async def subscribe_to_door_sensor(ws):
     logging.info("Subscribing to door sensors...")
@@ -16,7 +16,7 @@ async def subscribe_to_door_sensor(ws):
         "type": "subscribe_trigger",
         "trigger": {
             "platform": "state",
-            "entity_id": DOOR_SENSOR,
+            "entity_id": SENSORS,
             "from": "off",
             "to": "on"
         }
@@ -27,9 +27,8 @@ async def subscribe_to_door_sensor(ws):
 
 async def handle_door_sensor_events(res, config_data, armed_mode):
     # Implement logic here
-    if ( json.loads(res)["event"]["variables"]["trigger"]["entity_id"] == DOOR_SENSOR and ( armed_mode.strip() == ArmedMode.ARMED_AWAY.value or armed_mode.strip() == ArmedMode.ARMED_HOME.value)):
+    if armed_mode.strip() in (ArmedMode.ARMED_AWAY.value, ArmedMode.ARMED_HOME.value):
         sensor = json.loads(res)["event"]["variables"]["trigger"]["to_state"]["attributes"]["friendly_name"]
         send_notification(config_data, sensor + " has been opened")
-
 
     
